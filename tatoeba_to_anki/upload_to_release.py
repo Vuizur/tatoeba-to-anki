53a9ffe91bca1362d59b2ffbbf16dc6ace56a387
Hannes Krumbiegel <Vuizur@users.noreply.github.com>
2023-02-21 23:52:06 +0100

    First version of automated deck upload

---
+++ tatoeba_to_anki/upload_to_release.py
@@ -0,0 +1,40 @@
+# This uploads all apkg files in the current directory to a release on GitHub.
+
+import github
+import dotenv
+import os
+import glob
+
+dotenv.load_dotenv()
+
+g = github.Github(os.getenv("GITHUB_TOKEN"))
+
+repo = g.get_repo("Vuizur/tatoeba-to-anki")
+
+# Now look if there is a release with the name "latest"
+# If yes, replace all files in that release with the new ones
+# If no, create a new release with the new files
+
+release = None
+for r in repo.get_releases():
+    if r.title == "latest":
+        release = r
+        break
+
+if release is None:
+    release = repo.create_git_release(
+        "latest",
+        "latest",
+        "latest",
+        draft=False,
+        prerelease=False,
+    )
+
+# Now delete all files in the release
+for asset in release.get_assets():
+    asset.delete_asset()
+
+# Now upload all files in the current directory
+for file in glob.glob("*.apkg"):
+    release.upload_asset(file)
+    
